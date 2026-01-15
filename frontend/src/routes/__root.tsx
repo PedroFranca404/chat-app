0ab8c02348fbda590acd540878339017548dcf26
luis605 <lusluistomascabetedealmeida@gmail.com>
2026-01-15 23:15:20 +0000

    feat: 404 Page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,11 @@
+// src/routes/__root.tsx
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { NotFoundPage } from '../components/NotFoundPage'
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFoundPage,
 })
 
 function RootComponent() {
