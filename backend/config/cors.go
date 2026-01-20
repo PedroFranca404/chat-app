1c7927aa6cc4d2ab89591b4cb38dd9845adb2327
Pedro França <78953836+PedroFranca404@users.noreply.github.com>
2026-01-20 11:49:49 +0000

    refactored cors in order to use env frontend url feat: added .env.example

--- backend/config/cors.go
+++ backend/config/cors.go
@@ -1,10 +1,18 @@
 package config
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Cors(next http.Handler) http.Handler {
+	frontendUrl := os.Getenv("FRONTEND_URL")
+	if frontendUrl == "" {
+		println("FRONTEND_URL is not set, using default")
+		frontendUrl = "http://localhost:5173"
+	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", frontendUrl)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
